d76ba1cf36b48b4407aa93ea7ef6de35b9eb38c3
Nick Spinale <nick@nickspinale.com>
2025-12-05 14:47:56 -0800

    x

--- syntax.py
+++ syntax.py
@@ -546,7 +546,7 @@ class Expr:
 			xs.append (''.join(['%02x' % ord(c) for c in repr(self.val)]))
 			self.typ.serialise (xs)
 		else:
-			print ("not ser", self.kind)
+			print ("not ser", self.kind, self.name)
 			assert not 'expr serialisable', self.kind
 
 class Struct:
